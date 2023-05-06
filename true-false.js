5a473bd749abdcfff77b306c996785cb889492ce
SaiyedulM-Error404 <mdsaiyedulmorsalin.code@gmail.comgit config --global user.email>
2023-05-06 22:05:17 +0600

    undefined vs null

--- true-false.js
+++ true-false.js
@@ -3,6 +3,13 @@
 // const age = 0;
 // const name = "";
 // False is true that condition 0,"",nul,NaN.......it's always false
+/*Falsy value:
+    0 or "" = undefined.
+    nul,NaN,false
+Truthy value:
+" ",1,
+*/
+
 
 let name = null;
 console.log(name);
