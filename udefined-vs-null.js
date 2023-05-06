f37402bb305c0ee88ca25ce35efd06caa3150dd7
SaiyedulM-Error404 <mdsaiyedulmorsalin.code@gmail.comgit config --global user.email>
2023-05-06 23:10:50 +0600

    null vs undefined concept clear for interview

--- udefined-vs-null.js
+++ udefined-vs-null.js
@@ -1,15 +1,15 @@
-// let pakhi;
-// console.log(pakhi);
-// function add(num1, num2){
-//     console.log(num1 + num2);
-//     return;
-// }
+let pakhi;
+console.log(pakhi);
+function add(num1, num2){
+    console.log(num1 + num2);
+    return;
+}
 
-// function add(n,n2){
-//     console.log(n2);
-// }
-// const result = add(20,20);
-// console.log(result);
+function add(n,n2){
+    console.log(n2);
+}
+const result = add(20,20);
+console.log(result);
 
 const premik = {
     name:"smart dude", phone:98345
