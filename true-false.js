a5c952795d15dae9b64ae4f09bb4d26f97e416ac
SaiyedulM-Error404 <mdsaiyedulmorsalin.code@gmail.comgit config --global user.email>
2023-05-06 21:46:54 +0600

    This value is always false

--- true-false.js
+++ true-false.js
@@ -1,8 +1,12 @@
 
 
-const age = 0;
-const name = ""
-if(name.length>0){
+// const age = 0;
+// const name = "";
+// False is true that condition 0,"",nul,NaN.......it's always false
+
+let name = null;
+console.log(name);
+if(name){
     console.log("Condition is true");
 }
 else{
