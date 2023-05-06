4f39984c47c000570757ee12258ba1bc9a623a5f
SaiyedulM-Error404 <mdsaiyedulmorsalin.code@gmail.comgit config --global user.email>
2023-05-06 23:04:20 +0600

    undefined vs null last update

---
+++ udefined-vs-null.js
@@ -0,0 +1,12 @@
+let pakhi;
+console.log(pakhi);
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return;
+// }
+
+function add(n,n2){
+    console.log(n2);
+}
+const result = add(20,20);
+console.log(result);
