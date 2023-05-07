57a5b6c6a478a1c1f0d81ec0de2ca415d9b2178a
SaiyedulM-Error404 <mdsaiyedulmorsalin.code@gmail.comgit config --global user.email>
2023-05-07 10:28:37 +0600

    Double vs Tripple Equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,12 @@
+const first = 2;
+// const second = 3;
+const second = "2";
+
+// Equal means........one = assign a value, two == check the value, three === check the 
+
+if(first === second){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
