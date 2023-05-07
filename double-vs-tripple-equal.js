94cad2bde0121b1b82c76d97105fb32fd2a6855b
SaiyedulM-Error404 <mdsaiyedulmorsalin.code@gmail.comgit config --global user.email>
2023-05-07 10:35:35 +0600

    last updated == vs ===.

--- double-vs-tripple-equal.js
+++ double-vs-tripple-equal.js
@@ -1,11 +1,20 @@
-const first = 2;
-// const second = 3;
-const second = "2";
+let first = 2;
+let second = 3;
+ second = "2";
+ first = 1;
+ second = true
+ first = 0;
+ second = false;
 
-// Equal means........one = assign a value, two == check the value, three === check the 
+
+
+// Equal means........one = assign a value, two == check the value, three === check the value and types...
 
 if(first === second){
     console.log("Condition is true");
+}
+else if(first == second){
+    console.log("Condition is true but when double equal with string");
 }
 else{
     console.log("Condition is false");
